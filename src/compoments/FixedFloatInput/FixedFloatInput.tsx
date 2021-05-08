9a47bf126b33a7e1e9f39ee005280c460ad53c9d
pamoller <kontakt@pamoller.com>
2021-05-08 18:51:15 +0200

    optimizations

--- src/compoments/FixedFloatInput/FixedFloatInput.tsx
+++ src/compoments/FixedFloatInput/FixedFloatInput.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import PropTypes, { InferProps } from 'prop-types';
 
-const defaultFormatter = function(precision: number) {
-    precision = Math.abs(Math.ceil(precision<1?0:precision));
+const defaultFormatter = function (precision: number) {
+    precision = Math.abs(Math.ceil(precision < 1 ? 0 : precision));
     return number => (Math.round(10 ** precision * parseFloat(number)) / 10 ** precision).toFixed(precision);
 }
+
 
 const propTypes = {
     value: PropTypes.number.isRequired,
@@ -21,7 +22,9 @@ const propTypes = {
     name: PropTypes.string,
     readonly: PropTypes.bool,
     required: PropTypes.bool,
-    step: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+    step: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    className: PropTypes.string,
+    id: PropTypes.string
 };
 
 const defaultProps = {
@@ -30,7 +33,7 @@ const defaultProps = {
     onKeyPress: () => undefined
 };
 
-function FixedFloatInput({value, onChange, precision, formatter, onBlur, onKeyPress, ...props}: InferProps<typeof FixedFloatInput.propTypes>) {
+function FixedFloatInput({ value, onChange, precision, formatter, onBlur, onKeyPress, ...props }: InferProps<typeof FixedFloatInput.propTypes>)  {
     const format = formatter ? formatter : defaultFormatter(precision);
     const [innerValue, setInnerValue] = useState(format(value));
 
@@ -38,7 +41,7 @@ function FixedFloatInput({value, onChange, precision, formatter, onBlur, onKeyPr
         setInnerValue(value);
         onChange(parseFloat(value), event);
     }
-    
+
     function onInnerChange(event) {
         setAllValues(event.target.value, event);
     }
@@ -48,9 +51,9 @@ function FixedFloatInput({value, onChange, precision, formatter, onBlur, onKeyPr
             setAllValues(format(innerValue), event);
         onBlur(event.target.value, event)
     }
-    
+
     function onInnerKeyPress(event) {
-        if (['-','0','1','2','3','4','5','6','7','8','9',',','.'].indexOf(event.key) < 0)
+        if (['-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',', '.'].indexOf(event.key) < 0)
             event.preventDefault();
         onKeyPress(event);
     }
@@ -68,7 +71,7 @@ function FixedFloatInput({value, onChange, precision, formatter, onBlur, onKeyPr
         () => {
             setInnerValue(format(innerValue));
         },
-        [precision] 
+        [precision]
     );
 
     return (
